fedc0194888f252df60d50f0daf32556d439e042
Cedric Hauber <cedric.hauber@wescale.fr>
2016-02-23 16:36:33 +0100

    Add rundeck ssl configuration

--- rundeck/entrypoint.sh
+++ rundeck/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-. /etc/rundeck/profile
 confd -backend env -onetime
-${JAVA_HOME:-/usr}/bin/java ${RDECK_JVM} -cp ${BOOTSTRAP_CP} com.dtolabs.rundeck.RunServer /var/lib/rundeck ${RDECK_HTTP_PORT}
+
+. /etc/rundeck/profile
+RDECK_JVM="$RDECK_JVM -Drundeck.ssl.config=/etc/rundeck/ssl/ssl.properties -Dserver.https.port=${RDECK_HTTPS_PORT}"
+${JAVA_HOME:-/usr}/bin/java ${RDECK_JVM} -cp ${BOOTSTRAP_CP} com.dtolabs.rundeck.RunServer /var/lib/rundeck ${RDECK_HTTPS_PORT}
+
 
