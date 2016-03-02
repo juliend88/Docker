b5ef3ce50e95c703e5b9e06850140ff353e533e1
Cedric Hauber <cedric.hauber@wescale.fr>
2016-03-02 18:19:32 +0100

    FIx toml Add confd prefix setting

--- aptly/entrypoint.sh
+++ aptly/entrypoint.sh
@@ -4,7 +4,7 @@ if [ ! -f /etc/aptly.conf ]
 then
     if [ -n "$ETCD_NODE" ]
     then
-        confd -backend etcd -node ${ETCD_NODE} -onetime
+        confd -backend etcd -node ${ETCD_NODE} -prefix ${ETCD_PREFIX} -onetime
     else
         confd -backend env -onetime
     fi
--- rundeck/entrypoint.sh
+++ rundeck/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -n "$ETCD_NODE" ]
 then
-    confd -backend etcd -node ${ETCD_NODE} -onetime
+    confd -backend etcd -node ${ETCD_NODE} -prefix ${ETCD_PREFIX} -onetime
 else
     confd -backend env -onetime
 fi
