cb979075980c96382b169d88582ad1a82220999a
Cedric Hauber <cedric.hauber@wescale.fr>
2016-03-08 17:50:58 +0100

    Fix inventory

--- ansible/entrypoint.sh
+++ ansible/entrypoint.sh
@@ -2,7 +2,15 @@
 
 
 cat <<EOF > /etc/ansible/hosts
-${SERVER_ID} ansible_ssh_host=${SERVER_IP} ansible_ssh_private_key_file='/keypair.pem' ansible_ssh_user='cloud' toolbox_address=${TOOLBOX_IP} openstack_id=${SERVER_ID} server_ip=${SERVER_IP}
+[servers]
+${SERVER_ID}
+[servers:vars]
+ansible_ssh_host=${SERVER_IP}
+ansible_ssh_private_key_file='/keypair.pem'
+ansible_ssh_user='cloud'
+toolbox_address=${TOOLBOX_IP}
+openstack_id=${SERVER_ID}
+server_ip=${SERVER_IP}
 EOF
 
 bash -c "$*"
