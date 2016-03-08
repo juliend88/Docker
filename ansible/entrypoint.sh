ae0bc59025dae9192cdc34d73ef94c7a7bf7f126
Cedric Hauber <cedric.hauber@wescale.fr>
2016-03-08 17:45:53 +0100

    Simplify mechanism

--- ansible/entrypoint.sh
+++ ansible/entrypoint.sh
@@ -1,16 +1,8 @@
 #!/usr/bin/env bash
 
-cat <<EOF > /etc/ansible/openstack.yml
-clouds:
-  cloudwatt:
-    auth:
-      auth_url: $OS_AUTH_URL
-      username: $OS_USERNAME
-      password: $OS_PASSWORD
-      project_name: $OS_PROJECT_NAME
-ansible:
-  use_hostnames: True
-  expand_hostvars: False
+
+cat <<EOF > /etc/ansible/hosts
+${SERVER_ID} ansible_ssh_host=${SERVER_IP} ansible_ssh_private_key_file='/keypair.pem' ansible_ssh_user='cloud' toolbox_address=${TOOLBOX_IP} openstack_id=${SERVER_ID} server_ip=${SERVER_IP}
 EOF
 
 bash -c "$*"
