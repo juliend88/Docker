ee57a5610f63d075d63d386aacf6c0884ddfa3a3
Cedric Hauber <cedric.hauber@wescale.fr>
2016-04-28 10:40:54 +0200

    Fix merge

--- aptly/entrypoint.sh
+++ aptly/entrypoint.sh
@@ -23,16 +23,11 @@ then
          gpg --no-default-keyring --keyring trustedkeys.gpg --keyserver keys.gnupg.net --recv-keys 2A194991
 fi
 
-<<<<<<< Updated upstream
 MIRROREXIST=$(aptly mirror list)
 if [ "$MIRROREXIST"="No mirrors found*" ]
 then
     echo "Mirror does not exist"
     ./etc/cron.daily/deb-mirror &
-=======
-if `aptly mirror list` == "*No mirrors found*"; then
-    ./etc/cron.daily/deb-mirror
->>>>>>> Stashed changes
 fi
 
 bash -c "$*"
