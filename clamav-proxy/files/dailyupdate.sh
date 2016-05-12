53865d8a84de1a0ea022a3228809455db0907e22
Cedric Hauber <cedric.hauber@wescale.fr>
2016-05-12 10:32:06 +0200

    Improve freshclam

--- clamav-proxy/files/dailyupdate.sh
+++ clamav-proxy/files/dailyupdate.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-wget -O /usr/share/nginx/html/daily.cvd http://database.clamav.net/daily.cvd
-wget -O /usr/share/nginx/html/main.cvd http://database.clamav.net/main.cvd
+until [ $(stat -c '%s' "/usr/share/nginx/html/daily.cvd") -gt 0 ]; do
+    wget -c -O /usr/share/nginx/html/daily.cvd http://database.clamav.net/daily.cvd
+done
+until [ $(stat -c '%s' "/usr/share/nginx/html/main.cvd") -gt 0 ]; do
+    wget -c -O /usr/share/nginx/html/main.cvd http://database.clamav.net/main.cvd
+done
