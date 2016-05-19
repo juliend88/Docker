f15a4236226d4c48c111b82308fd27b52294cb6c
Cedric Hauber <cedric.hauber@wescale.fr>
2016-05-19 10:15:44 +0200

    Upgrade syncthing and fix graylog

--- syncthing/reload.sh
+++ syncthing/reload.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ -f "/initialized" ]
 then
-    until [ -n "$(curl -H 'X-API-Key: 8EqKansuOM1TQPt3O3aJs-tDlMdlTpLF' --silent "http://localhost:8384/rest/system/status")" ]
+    until [ "$(curl -H 'X-API-Key: 8EqKansuOM1TQPt3O3aJs-tDlMdlTpLF' --write-out %{http_code} --silent --output /dev/null "http://localhost:8384/rest/system/status")" -eq "200" ]
     do
-      echo "Trying: http://localhost:8384"
+      echo "Waiting for Syncthing API"
       sleep 1
     done
     /usr/bin/curl -X POST -H 'X-API-Key: 8EqKansuOM1TQPt3O3aJs-tDlMdlTpLF' http://localhost:8384/rest/system/restart
