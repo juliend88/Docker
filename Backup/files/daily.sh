4a5531f15caecd6f52bd2def1d6bfae105743e93
Xcite <julien.deplaix@cloudwatt.com>
2016-07-04 15:17:39 +0200

    Add Backup Container

--- Backup/files/daily.sh
+++ Backup/files/daily.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-IP=$1
-DIRECTORY=$2
-for dir in $DIRECTORY
-do
-  sshfs cloud@$IP:$dir /mnt/$dir -o IdentityFile=/secret/key.pem -o StrictHostKeyChecking=no
-done
-
 src=/mnt
 dest="swift://$1"
 
 duplicity --allow-source-mismatch --no-encryption --verbosity notice \
-        --full-if-older-than 7D \
-        --num-retries 3 \
-        --asynchronous-upload \
+          --full-if-older-than 3M \
+          --num-retries 3 \
+          --asynchronous-upload \
          "${src}" "${dest}"
