c7338f0bfd995a0b9433d6d06a90750d6addc23f
Xcite <julien.deplaix@cloudwatt.com>
2016-07-05 15:27:59 +0200

    Change run.sh

--- Backup/files/run.sh
+++ Backup/files/run.sh
@@ -3,23 +3,25 @@ ACTION=$1
 
 if [ "$ACTION" == "restore" ]
 then
-    IP=$2
+    IP=$3
+    ID=$2
     mkdir -p /mnt/restore
-    sshfs cloud@$IP:/restore/ /mnt/restore -o IdentityFile=/secret/key.pem -o StrictHostKeyChecking=no
+    sshfs $IP:/restore/ /mnt/restore -o IdentityFile=/secret/key.pem -o StrictHostKeyChecking=no
     echo $ACTION
-    exec ./restore.sh $IP
+    exec ./restore.sh $ID
 elif [ "$ACTION" == "backup" ]
 then
   PERIOD=$2
-  IP=$3
-  DIRECTORY=$4
+  ID=$3
+  IP=$4
+  DIRECTORY=$5
   for dir in $DIRECTORY
   do
     mkdir -p /mnt/$dir
-    sshfs cloud@$IP:$dir /mnt/$dir -o IdentityFile=/secret/key.pem -o StrictHostKeyChecking=no
+    sshfs $IP:$dir /mnt/$dir -o IdentityFile=/secret/key.pem -o StrictHostKeyChecking=no
   done
   echo $ACTION
-  exec ./$PERIOD.sh $IP
+  exec ./$PERIOD.sh $ID
 else
   exec $*
 fi
