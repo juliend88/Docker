7d7cd27fe4f4ed5d3ce34d51f460aae0a35986d6
Xcite <julien.deplaix@cloudwatt.com>
2016-07-05 10:29:46 +0200

    Backup container

--- Backup/files/daily.sh
+++ Backup/files/daily.sh
@@ -3,7 +3,7 @@ src=/mnt
 dest="swift://$1"
 
 duplicity --allow-source-mismatch --no-encryption --verbosity notice \
-          --full-if-older-than 3M \
+          --full-if-older-than 7D \
           --num-retries 3 \
           --asynchronous-upload \
          "${src}" "${dest}"
--- Backup/files/run.sh
+++ Backup/files/run.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
-IP=$2
-DIRECTORY=$3
-ACTION=$4
-for dir in $DIRECTORY
-do
-  sshfs cloud@$IP:$dir /mnt/$dir -o IdentityFile=/secret/key.pem -o StrictHostKeyChecking=no
-done
+ACTION=$1
 
- if [  "$ACTION" != "backup" ]
- then
-     sshfs cloud@$IP:/restore/ /mnt/restore -o IdentityFile=/secret/key.pem -o StrictHostKeyChecking=no
-   echo $ACTION
-   exec ./restore.sh $2
- else
-   for dir in $DIRECTORY
-   do
-     sshfs cloud@$IP:$dir /mnt/$dir -o IdentityFile=/secret/key.pem -o StrictHostKeyChecking=no
-   done
-   echo $ACTION
-   exec ./$1.sh $2 $3
- fi
+if [ "$ACTION" == "restore" ]
+then
+    IP=$2
+    mkdir -p /mnt/restore
+    sshfs cloud@$IP:/restore/ /mnt/restore -o IdentityFile=/secret/key.pem -o StrictHostKeyChecking=no
+    echo $ACTION
+    exec ./restore.sh $IP
+elif [ "$ACTION" == "backup" ]
+then
+  PERIOD=$2
+  IP=$3
+  DIRECTORY=$4
+  for dir in $DIRECTORY
+  do
+    mkdir -p /mnt/$dir
+    sshfs cloud@$IP:$dir /mnt/$dir -o IdentityFile=/secret/key.pem -o StrictHostKeyChecking=no
+  done
+  echo $ACTION
+  exec ./$PERIOD.sh $IP
+else
+  exec $*
+fi
