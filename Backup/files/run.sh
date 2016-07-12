e10509f16f2122a22c8e55754a4a51443c29d678
Cedric Hauber <cedric.hauber@wescale.fr>
2016-07-13 01:20:08 +0200

    Fix backup Fix stack bugs

--- Backup/files/run.sh
+++ Backup/files/run.sh
@@ -7,10 +7,12 @@ if [ "$ACTION" == "restore" ]
 then
     IP=$3
     ID=$2
+    REF=$4
     mkdir -p /mnt/restore
+    ssh -o StrictHostKeyChecking=no -o IdentityFile=/secret/key.pem $IP -C "sudo mkdir -p /restore; sudo chown \$USER:\$USER /restore" || true
     sshfs $IP:/restore/ /mnt/restore -o IdentityFile=/secret/key.pem -o StrictHostKeyChecking=no
     echo $ACTION
-    exec ./restore.sh $ID
+    exec ./restore.sh $ID $REF
 elif [ "$ACTION" == "backup" ]
 then
   PERIOD=$2
@@ -20,6 +22,7 @@ then
   for dir in $DIRECTORY
   do
     mkdir -p /mnt/$dir
+    ssh -o StrictHostKeyChecking=no -o IdentityFile=/secret/key.pem $IP -C "sudo mkdir -p $dir; sudo chown \$USER:\$USER $dir" || true
     sshfs $IP:$dir /mnt/$dir -o IdentityFile=/secret/key.pem -o StrictHostKeyChecking=no
   done
   echo $ACTION
