4e3fe31550f0593912836bffe91f0e2eed50f240
Cedric Hauber <cedric.hauber@wescale.fr>
2016-11-23 17:56:03 +0100

    Prepare for MCM v3

--- Backup/files/run.sh
+++ Backup/files/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+chmod 0600 /keys/ssh-privatekey
 
 ACTION=$1
 
@@ -9,8 +10,8 @@ then
     ID=$2
     REF=$4
     mkdir -p /mnt/restore
-    ssh -o StrictHostKeyChecking=no -o IdentityFile=/secret/key.pem $IP -C "sudo mkdir -p /restore; sudo chown \$USER:\$USER /restore" || true
-    sshfs $IP:/restore/ /mnt/restore -o IdentityFile=/secret/key.pem -o StrictHostKeyChecking=no
+    ssh -o StrictHostKeyChecking=no -o IdentityFile=/keys/ssh-privatekey $IP -C "sudo mkdir -p /restore; sudo chown \$USER:\$USER /restore" || true
+    sshfs $IP:/restore/ /mnt/restore -o IdentityFile=/keys/ssh-privatekey -o StrictHostKeyChecking=no
     echo $ACTION
     exec ./restore.sh $ID $REF
 elif [ "$ACTION" == "backup" ]
@@ -22,8 +23,8 @@ then
   for dir in $DIRECTORY
   do
     mkdir -p /mnt/$dir
-    ssh -o StrictHostKeyChecking=no -o IdentityFile=/secret/key.pem $IP -C "sudo mkdir -p $dir; sudo chown \$USER:\$USER $dir" || true
-    sshfs $IP:$dir /mnt/$dir -o IdentityFile=/secret/key.pem -o StrictHostKeyChecking=no
+    ssh -o StrictHostKeyChecking=no -o IdentityFile=/keys/ssh-privatekey $IP -C "sudo mkdir -p $dir; sudo chown \$USER:\$USER $dir" || true
+    sshfs $IP:$dir /mnt/$dir -o IdentityFile=/keys/ssh-privatekey -o StrictHostKeyChecking=no
   done
   echo $ACTION
   exec ./$PERIOD.sh $ID
